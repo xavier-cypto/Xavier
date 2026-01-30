a273fca23933e9d77ea1eb8574ae35f4d40a5753
xavier-cypto <batoneke06@gmail.com>
2026-01-30 01:37:52 -0800

    Update contributions.ts

--- config/contributions.ts
+++ config/contributions.ts
@@ -10,15 +10,15 @@ export const contributionsUnsorted: contributionsInterface[] = [
     repo: "minimal-next-portfolio",
     contibutionDescription:
       "Open-source Next.js portfolio template. Trusted and forked by developers worldwide (130+ GitHub stars).",
-    repoOwner: "namanbarkiya",
-    link: "https://github.com/namanbarkiya/minimal-next-portfolio",
+    repoOwner: "Xavier",
+    link: "https://github.com/xavier-cypto/xavier",
   },
   {
     repo: "niya-saas-template",
     contibutionDescription:
       "Modern Next.js SaaS template. Production-ready starter for devs and AI startups (30+ GitHub stars).",
-    repoOwner: "namanbarkiya",
-    link: "https://github.com/namanbarkiya/niya-saas-template",
+    repoOwner: "Xavier",
+    link: "https://github.com/xavier-cypto/niya-saas-template",
   },
   {
     repo: "autogen",
