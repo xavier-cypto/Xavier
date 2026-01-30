233b424396b06f12a10a69d8803c9a5ca1421802
xavier-cypto <batoneke06@gmail.com>
2026-01-30 00:37:01 -0800

    Initial commit

---
+++ config/routes.ts
@@ -0,0 +1,24 @@
+export const routesConfig: any = {
+  mainNav: [
+    {
+      title: "Projects",
+      href: "/projects",
+    },
+    {
+      title: "Experience",
+      href: "/experience",
+    },
+    {
+      title: "Contributions",
+      href: "/contributions",
+    },
+    {
+      title: "Skills",
+      href: "/skills",
+    },
+    {
+      title: "Contact",
+      href: "/contact",
+    },
+  ],
+};
