e7a63edb5d0289554d25ed357a73e2f40b93ed64
xavier-cypto <batoneke06@gmail.com>
2026-01-30 01:31:38 -0800

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -24,7 +24,7 @@ import profileImg from "@/public/profile-img.jpg";
 export const metadata: Metadata = {
   title: `${pagesConfig.home.metadata.title}`,
   description:
-    "Naman Barkiya - Applied AI Engineer working at the intersection of AI, data, and scalable software systems. Explore my projects, experience, and contributions.",
+    "Francois xavier NZABAMWITA - Applied AI Engineer working at the intersection of AI, data, and scalable software systems. Explore my projects, experience, and contributions.",
   alternates: {
     canonical: siteConfig.url,
   },
@@ -82,7 +82,7 @@ export default function IndexPage() {
             width={100}
             sizes="100vw"
             className="bg-primary rounded-full mb-0 h-auto md:mb-2 w-[60%] max-w-[16rem] border-8 border-primary"
-            alt="Naman Barkiya - Applied AI Engineer Portfolio"
+            alt="Francois xavier NZABAMWITA - Applied AI Engineer Portfolio"
             priority
           />
           <AnimatedText
@@ -90,7 +90,7 @@ export default function IndexPage() {
             delay={0.2}
             className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl"
           >
-            Naman Barkiya
+            Francois xavier NZABAMWITA
           </AnimatedText>
           <AnimatedText
             as="h3"
@@ -127,7 +127,7 @@ export default function IndexPage() {
                     size: "lg",
                   })
                 )}
-                aria-label="Contact Naman Barkiya"
+                aria-label="Contact Francois xavier NZABAMWITA"
               >
                 <Icons.contact className="w-4 h-4 mr-2" /> Contact
               </Link>
